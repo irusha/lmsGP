a7ea988db8cdc0a988b3104492f5bab59d286b03
cakesbysahas <115912585+cakesbysahas@users.noreply.github.com>
2022-10-29 17:22:22 +0530

    New drop down menu for hidden nav bar options

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -1,5 +1,7 @@
 import './App.css'
 
+
+
 export default function () {
     return (
         <nav className="navbar2">
@@ -8,16 +10,42 @@ export default function () {
 
                 <div className="menu2">
                     <li><a href="/">A. L. Jayasingha</a></li>
-                    <li><a href="/">Study room</a></li>
+                    <li><a href="/">Study Room</a></li>
                     <li id="lastOption"><a href="/">Sign Out</a></li>
                 </div>
             </ul>
-            
 
-            <span className="material-symbols-outlined menubutton ">
+
+            <span className="material-symbols-outlined menubutton" id='menuB' onClick={expandMenu}>
                 menu
             </span>
+
+            <div class="dropdown-content" id='dropDown'>
+                <p href="#">A.L. Jayasingha</p>
+                <p href="#">Study Room</p>
+                <p href="#">Sign Out</p>
+            </div>
+
             
+
         </nav>
     )
+}
+
+let isMenuExpanded = false
+
+function expandMenu(){
+    var dropDownMenu = document.getElementById('dropDown')
+    var menuB = document.getElementById('menuB')
+    
+    if(isMenuExpanded){
+        dropDownMenu.setAttribute("style", "display:none")
+        menuB.textContent = "menu"
+    }
+    else{
+        dropDownMenu.setAttribute("style", "display:block")
+        menuB.textContent = "close"
+    }
+
+    isMenuExpanded = !isMenuExpanded
 }
